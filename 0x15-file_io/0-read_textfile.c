c143962bd33e6afd13d9ea7a239dabc20ffb09ac
Andrews0899 <andrewsmakafui350@gmail.com>
2023-05-08 20:49:09 -0700

    some errors corrected

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- * 0 when function fails or filename is NULL.
+ * read_textfile- Read file print to STDOUT.
+ * @filename: text  being read
+ * @letters: letters to be read
+ * Return: w- actual number of bytes  printed
+ * 0  filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
+ * append_text_to_file - this function appends text to a file.
+ * @filename:  pointer to the file of interest.
+ * @text_content:  string to append to the file.
  *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
+ * Return: If the  filename is NULL - -1.
  *         Otherwise - 1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int O, W, len = 0;
 
 if (filename == NULL)
 return (-1);
@@ -22,13 +21,13 @@ for (len = 0; text_content[len];)
 len++;
 }
 
-o = open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
+O = open(filename, O_WRONLY | O_APPEND);
+W = write(O, text_content, len);
 
-if (o == -1 || w == -1)
+if (O == -1 || W == -1)
 return (-1);
 
-close(o);
+close(O);
 
 return (1);
 }
