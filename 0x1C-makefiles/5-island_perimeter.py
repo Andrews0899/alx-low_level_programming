b268e55cf4f2c054925059af33ef59acf32e7823
Andrews0899 <andrewsmakafui350@gmail.com>
2023-08-23 18:37:45 -0700

    Island parimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-""" Grid Mobule """
-
 
 def island_perimeter(grid):
-    """ looks for any islands inside grid and calculates it's perimeter """
-    width = 0
-    height = 0
-    height_idx = 0
-    for i in range(len(grid)):
-        j = height_idx
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if grid[i][j+1] == 1:
-                    width += 1
-                    continue
-                elif grid[i+1][j] == 1:
-                    height += 1
-                    height_idx = j
-                    break
-                else:
-                    if width == 0 and grid[i][j-1] == 1:
-                        j = 0
-                        continue
-                    width += 1
-                    height += 1
-                    break
-    return (width + height) * 2
-
+    """
+    Calculate the perimeter of the island described in the grid.
+    
+    Args:
+    grid (list[list[int]]): The grid representing the island.
+    
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:  # If it's land
+                perimeter += 4  # Add the base perimeter
+                
+                # Check adjacent cells and subtract if they are land
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
